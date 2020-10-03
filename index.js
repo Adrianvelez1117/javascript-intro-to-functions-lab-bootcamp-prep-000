a1a807bdbaf5c9c7a40552ac546df56e50b78464
stellar-routine-2744 <adrianvelez1117@gmail.com>
2020-10-03 17:46:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
   
-function logShout(hello) {}
-function logShout(string) {}
-  console.log() string.toUpperCase()
+function logShout('hello') {}
+function logShout('hello') {}
+  console.log() logShout.toUpperCase()
 }
 
